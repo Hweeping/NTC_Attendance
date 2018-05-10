dcc2a30b055ff6492f982b122bb918219c40cb39
Hweeping <hweeping_dnt@hotmail.com>
2018-05-10 15:59:08 +0800

    Create simple input fields

--- app.R
+++ app.R
@@ -1,5 +1,16 @@
 library(shiny)
+library(shinyjs)
 library(shinydashboard)
+
+fieldsMandatory <- c("name", "age")
+
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+appCSS <- ".mandatory_star { color: red; }"
 
 ui <- dashboardPage(
   dashboardHeader(
@@ -16,13 +27,40 @@ ui <- dashboardPage(
     tabItems(
       #dashboard tab
       tabItem(tabName = "dashboard", h2("Overview")),
-      tabItem(tabName = "new_input", h2("New Data Entry")),
+      tabItem(
+        tabName = "new_input", 
+        h2("New Data Entry"),
+        fluidPage(
+          shinyjs::useShinyjs(),
+          shinyjs::inlineCSS(appCSS),
+          div(
+            id = "form",
+            textInput("name", labelMandatory("Name"), ""),
+            textInput("age", labelMandatory("Age"), ""),
+            actionButton("submit", "Submit", class = "btn-primary")
+          )
+        )
+      ),
       tabItem(tabName = "attendance", h2("Events Check-In"))
     )
   )
 )
 
-server <- function(input, output) {}
+server <- function(input, output, session) {
+  observe({
+    # check if all mandatory fields have a value
+    mandatoryFilled <-
+      vapply(fieldsMandatory,
+             function(x) {
+               !is.null(input[[x]]) && input[[x]] != ""
+             },
+             logical(1))
+    mandatoryFilled <- all(mandatoryFilled)
+    
+    # enable/disable the submit button
+    shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+  })
+}
 
 shinyApp(ui, server)
 
