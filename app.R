93f4b266450ef2d6c160d8ec54a73debaf9542f0
Hweeping <hweeping_dnt@hotmail.com>
2018-05-10 13:58:20 +0800

    First simple version

---
+++ app.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(
+    title = "Discovery Kids 2018"),
+  dashboardSidebar(
+    sidebarMenu(
+      id = "tabs",
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("New Input", tabName = "new input", icon = icon("pencil"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      #dashboard tab
+      tabItem(tabName = "dashboard", h2("Overview")),
+      tabItem(tabName = "new input", h2("New Data Entry"))
+    )
+  )
+)
+
+server <- function(input, output) {}
+
+shinyApp(ui, server)
+
